45ebce7299bb7cdfd8ffbf943151202d3bffe3cd
Arina2246 <arinanebylova@gmail.com>
2022-05-15 14:03:47 +0300

    feat: add solution of read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.join(__dirname, 'text.txt');
+const readableFile = fs.createReadStream(`${pathToFile}`, 'utf-8');
+let data = '';
+readableFile.on('data', (chunk) => (data += chunk));
+readableFile.on('end', () => console.log('End', data));
